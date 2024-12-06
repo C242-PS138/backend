a0c98c9a7b60dedffb5526fd8d2a63631ef223cd
OgaJira <ogajirapaltarest@student.uir.ac.id>
2024-12-07 05:50:06 +0700

    belum fix 3

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.get("/auth/logout", (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
