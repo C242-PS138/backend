2e77291afe581c81596be91626f8dfb2d5eea6c3
syronesdreamland <116932673+syronesdreamland@users.noreply.github.com>
2024-12-10 05:29:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -143,15 +143,16 @@ app.listen(PORT,'0.0.0.0', () => {
 
 const connectDB = async () => {
     try {
-      await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log('MongoDB connected successfully');
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000 // Increase timeout to 5 seconds
+        });
+        console.log('MongoDB connected successfully');
     } catch (error) {
-      console.error('Error connecting to MongoDB:', error.message);
-      process.exit(1); // Exit with failure
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1);
     }
-  };
+};
 
 module.exports = connectDB;
